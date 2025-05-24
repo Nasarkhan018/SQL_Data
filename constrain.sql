63178e9cac6952a58604c1adff99eaf717b2e217
Nasarkhan <85803172+Nasarkhan018@users.noreply.github.com>
2025-05-24 12:28:39 +0500

    Add files via upload

--- constrain.sql
+++ constrain.sql
@@ -1,2 +1,38 @@
 -- constraints 
 -- condition that can apply on columns of table and these condition can be followed while inserting records into the table 
+-- not null constarint
+CREATE DATABASE constraints
+use constraints
+-- case 1 we have to create a new table 
+create table test_not_null(
+EID int not null,
+age tinyint,
+first_name varchar(256)
+)
+select * from test_not_null
+insert into test_not_null (EID,age,first_name) values (2,20,'Muhammad usman')
+===============================================================================>
+
+- case 2 the table already exist 
+alter table test_not_null
+alter column first_name varchar(256) not null
+insert into test_not_null (21,34,null)
+================================================================================>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
